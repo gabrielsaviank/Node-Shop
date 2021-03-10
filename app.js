2982727dac9a71eef73b106b82c77ad2d0cfe3a2
GabrielSavian <gabriel@amplimed.com.br>
2021-03-10 08:13:56 -0300

    bug fixing

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
-//Libraries and required stuff 
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//
 
 const app = express();
 
-//Routes Call
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-//
 
-//Routing Stuff 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname, '..', 'views', '404.html')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-//
 
-//Port Listen 
-app.listen(3000, function (params) {
-    console.log("ALLESYS: Server Started on Port 7000");
+app.listen(3000, function() {
+    console.log("AlleSys: Listening on port 3000");
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,3 @@
-//Libraries and Required Stuff
 const path = require('path');
 
 const express = require('express');
@@ -6,20 +5,17 @@ const express = require('express');
 const rootDir = require('../helpers/path');
 
 const router = express.Router();
-//
-
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
-//
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
-//
 
 module.exports = router;
+
