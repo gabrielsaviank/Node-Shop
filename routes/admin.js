731ebe4b40c46589b805ba49151f24908a10c4f3
GabrielSavian <gabriel@amplimed.com.br>
2021-03-10 07:56:43 -0300

    security commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+//Libraries and Required Stuff
 const path = require('path');
 
 const express = require('express');
@@ -5,16 +6,20 @@ const express = require('express');
 const rootDir = require('../helpers/path');
 
 const router = express.Router();
+//
+
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
+//
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
+//
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -6,10 +6,14 @@ const express = require('express');
 // This is the helps us with our paths.
 const rootDir = require('../helpers/path');
 
+//Router
 const router = express.Router();
+//
 
+//Get
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
+//
 
 module.exports = router;
