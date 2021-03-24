97184b5c230713da044d9ab008e76e7819b1a813
GabrielSavian <gabriel@amplimed.com.br>
2021-03-24 20:25:54 -0300

    adjusts

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,7 @@
 const Product = require('../models/product');
 
-//
 const getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
       pageTitle: 'Add Product',
       path: '/admin/add-product',
       formsCSS: true,
@@ -17,21 +16,5 @@ const postAddProduct = (req, res, next) => {
   res.redirect('/');
 };
 
-
-const getProducts = (req, res, next) => {
-  const products = Product.fetchAll((products) => {
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
-  })
-};
-
-
 exports.getAddProduct = getAddProduct;
 exports.postAddProduct = postAddProduct;
-exports.getProducts = getProducts;
